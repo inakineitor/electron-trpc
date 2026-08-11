688bca048e9106cc5cbc65066876e27d2c9ef0e6
Iñaki Arango <arangoinaki@gmail.com>
2026-08-10 17:52:38 -0700

    [spr] changes introduced through rebase
    Created using jj-spr 0.1.0
    
    [skip ci]

--- packages/electron-trpc/src/main/vite.config.ts
+++ packages/electron-trpc/src/main/vite.config.ts
@@ -1,20 +1,17 @@
 /// <reference types="vitest" />
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'path';
 import { defineConfig } from 'vite';
 
-const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({
+module.exports = defineConfig({
   base: './',
   build: {
     lib: {
-      entry: path.resolve(currentDirectory, './index.ts'),
+      entry: path.resolve(__dirname, './index.ts'),
       name: 'electron-trpc',
       formats: ['es', 'cjs'],
       fileName: (format) => ({ es: 'main.mjs', cjs: 'main.cjs' })[format as 'es' | 'cjs'],
     },
-    outDir: path.resolve(currentDirectory, '../../dist'),
+    outDir: path.resolve(__dirname, '../../dist'),
     rollupOptions: {
       external: ['electron'],
     },
--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -7,13 +7,9 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'electron-trpc',
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/inakineitor/electron-trpc',
-        },
-      ],
+      social: {
+        github: 'https://github.com/jsonnull/electron-trpc',
+      },
     }),
   ],
 });
